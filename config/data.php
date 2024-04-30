d6f7c2da16da5838f6245b837f4329ec6a00efcb
Yoann <scellier.yoann@gmail.com>
2024-04-30 08:55:34 +0200

    Finalisation générateur de signature basé sur un tableau associatif

--- config/data.php
+++ config/data.php
@@ -4,5 +4,5 @@
 
 // création d"une variable qui reprend un tableau multidimensionnel associatif
 $_SESSION["users"] = array(
-    array("id" => 1, "prenom" => "Yoann", "nom" => "Scellier", "poste1" => "Dev 1", "poste2" => "Dev 2", "numero" => "0606060606", "mail" => "Yoann@gmail.com") // je prépare un tableau vierge pour définir une structure.    
+    array("id" => 1, "prenom" => "Christelle", "nom" => "Pommier Fischer", "poste1" => "Commissaire aux comptes", "poste2" => "Expert comptable - Associée", "numero" => "06 65 25 76 06", "mail" => "c.pommier@enumiere.com") // je prépare un tableau vierge pour définir une structure.    
 ); 
